408cbcd194fcfd67d81c297855870ad8d79c7458
usman zahid <lxshani63gmail.com>
2023-12-15 09:48:55 -0800

    my project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,66 @@
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+
+          <Image 
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section> 
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
+
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
+      </section>
+    </>
+  )
+}
---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,78 @@
+import Collection from '@/components/shared/Collection'
+import { Button } from '@/components/ui/button'
+import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
+import { auth } from '@clerk/nextjs'
+import Link from 'next/link'
+import React from 'react'
+
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
+
+  return (
+    <>
+      {/* My Tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/#events">
+              Explore More Events
+            </Link>
+          </Button>
+        </div>
+      </section>
+
+      <section className="wrapper my-8">
+        <Collection 
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={ordersPage}
+          urlParamName="ordersPage"
+          totalPages={orders?.totalPages}
+        />
+      </section>
+
+      {/* Events Organized */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/events/create">
+              Create New Event
+            </Link>
+          </Button>
+        </div>
+      </section>
+
+      <section className="wrapper my-8">
+        <Collection 
+          data={organizedEvents?.data}
+          emptyTitle="No events have been created yet"
+          emptyStateSubtext="Go create some now"
+          collectionType="Events_Organized"
+          limit={3}
+          page={eventsPage}
+          urlParamName="eventsPage"
+          totalPages={organizedEvents?.totalPages}
+        />
+      </section>
+    </>
+  )
+}
+
+export default ProfilePage
